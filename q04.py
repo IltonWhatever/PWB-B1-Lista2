ac28c05f6feb2d7ef15bc376a9b6c12272001ce6
IltonWhatever <101358506+IltonWhatever@users.noreply.github.com>
2023-04-23 11:01:29 -0300

    Create q04.py

---
+++ q04.py
@@ -0,0 +1,6 @@
+# 4- Escreva um programa em Python que leia um número inteiro e imprima se ele é positivo ou negativo.
+numero = float(input("Digite um numero: "))
+if numero >= 0:
+  print("Positivo!")
+else:
+  print("Negativo!")
