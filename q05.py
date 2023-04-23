552faeec0ac334c1887fcef92a2163aafe58a4a3
IltonWhatever <101358506+IltonWhatever@users.noreply.github.com>
2023-04-23 11:06:38 -0300

    Create q05.py

---
+++ q05.py
@@ -0,0 +1,7 @@
+# 5- Escreva um programa em Python que leia dois números inteiros e imprima o maior deles.
+numero1 = float(input("Digite o primeiro numero: "))
+numero2 = float(input("Digite o segundo numero: "))
+if numero1 > numero2:
+  print("O numero maior é o primeiro numero! ",numero1)
+else:
+  print("O numero maior é o segundo numero! ",numero2)
