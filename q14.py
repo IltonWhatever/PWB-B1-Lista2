88bebc8fbdfcc77a95e90415812ab799b42f6312
IltonWhatever <101358506+IltonWhatever@users.noreply.github.com>
2023-04-23 11:12:57 -0300

    Create q14.py

---
+++ q14.py
@@ -0,0 +1,6 @@
+# 14- Escreva um programa em Python que leia uma lista de números e imprima a lista em ordem decrescente.
+lista = []
+tam = int(input("Digite o tamanho da lista: "))
+for n in range(tam):
+  lista.append(int(input("Digite o {0}º numero: ".format(n+1))))
+print("Lista ordenada:",sorted(lista, reverse=True))
